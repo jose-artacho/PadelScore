b7a2c9016d7e29e2ecf83e4013210eae10cebeea
Jose Artacho <jose.artacho@wefox.com>
2024-06-05 13:33:40 +0200

    first Commit

---
+++ PadelScore Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PadelScore Watch App
+//
+//  Created by Jose Artacho on 5/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PadelScore Watch App/PadelScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  PadelScoreApp.swift
+//  PadelScore Watch App
+//
+//  Created by Jose Artacho on 5/6/24.
+//
+
+import SwiftUI
+
+@main
+struct PadelScore_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
